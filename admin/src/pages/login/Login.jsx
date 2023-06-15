c95c3e00cbe3179ae524c287a0daca9fbe72d510
golam mostafa <gmnayeem25@gmail.com>
2023-06-16 03:17:06 +0600

    updated some code

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./login.css";
-import { useDispatch, useNavigate } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import {useNavigate} from 'react-router-dom';
 import { login } from "../../redux/apiCalls";
 import { loginSuccessful } from '../../redux/userReducer';
 import { toast, ToastContainer } from 'react-toastify';
